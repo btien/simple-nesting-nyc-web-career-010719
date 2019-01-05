c66d04ca11bb6fc68af2982b335c376423434708
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-05 22:18:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		   :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +78,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        } 
     
 end
 
